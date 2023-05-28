71e129674163102ec1e9f51a298b5fa8df45b273
1FarZ1 <91225280+1FarZ1@users.noreply.github.com>
2023-05-28 20:11:09 +0100

    auth almost done

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,24 +1,5 @@
 const User = require('../models/User')
 
-let login  = async  (req,res)=>{
-    const { email, password } = req.body;
-
-    try {
-        const user  = await User.find(user => user.email == email);
-        if(!user){
-            return res.status(400).json({ msg: 'no user found' })
-        }
-        if (password !== user.password) {
-            return res.status(400).json({ msg: 'incorrect password' })
-        }    
-
-    } catch (error) {
-        
-    }
-    
- 
-    res.status(200).json({ msg: 'user created', token })
-}
 
 let register = async (req,res)=>{
     const { username, email, password } = req.body;
@@ -28,9 +9,32 @@ let register = async (req,res)=>{
     let userData = {username,email,password} ; 
 
     const user =  await User.create(...userData);
-    user.createJwt();
+    let token =  user.createJwt();
 
-    res.status(200).json({ msg: 'user created', ...user })
+    res.status(200).json({ msg: 'user created',token });
+}
+let login  = async  (req,res)=>{    
+    try {
+
+        const { email, password } = req.body;
+        if (!email || !password) {
+            return res.status(400).json({ msg: 'fill all the credentials' })
+        }
+        const user  = await User.findOne(user => user.email == email);
+        if(!user){
+            return res.status(400).json({ msg: 'no user found' })
+        }
+        if (!user.comparePassword(password)) {
+            return res.status(400).json({ msg: 'incorrect password' })
+        }    
+        const token = user.createJwt();
+    
+        return res.status(200).json({ msg: 'user logged in successfully ', token })
+
+    } catch (error) {
+        return res.status(500).json({ msg: 'something went wrong',error })
+        
+    } 
 }
 
 module.exports = {
