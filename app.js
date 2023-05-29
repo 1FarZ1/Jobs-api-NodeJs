d2f6b1b1ce2f6b0900999d738474b166f9ce07a7
1FarZ1 <91225280+1FarZ1@users.noreply.github.com>
2023-05-29 19:35:18 +0100

    adding security

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 require('dotenv').config();
 const express = require('express');
-const {  connectDb } = require('./db/mongoDb');
+const { connectDb } = require('./db/mongoDb');
 
 const authRouter = require('./routes/auth');
 const jobsRouter = require('./routes/jobs');
+
+const helmet = require('helmet');
+const cors = require('cors');
+const xss = require('xss-clean');
+const rate_limiter = require('express-rate-limit');
 const authMiddleware = require('./middlewares/auth');
 
 
@@ -12,11 +17,26 @@ const port = process.env.PORT || 5000;
 
 app.use(express.static('./view'));
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
+
+app.use(helmet());
+app.use(cors());
+app.use(xss());
+app.use(
+    rate_limiter({
+        windowMs: 15 * 60 * 1000, // 15 minutes
+        max: 100
+    }));
 
 
-app.use("/api/v1/jobs",authMiddleware,jobsRouter);
-app.use("/api/v1/auth",authRouter);
+
+app.get("/", (req, res) => {
+    res.send("jobs api")
+});
+
+
+app.use("/api/v1/jobs", authMiddleware, jobsRouter);
+app.use("/api/v1/auth", authRouter);
 
 
 
@@ -24,26 +44,26 @@ app.use("/api/v1/auth",authRouter);
 
 
 
-app.use((req,res)=>{
+app.use((req, res) => {
     res.status(404).send("Page not found");
 }
-);  
-
-
-
-
-
-
-let main = async ()=>{  
-try {
-    await connectDb(process.env.MONGO_URI);
-    app.listen(port, () => {
-    console.log('Server is running on http://localhost:' + port);
-    }
 );
-} catch (error) {
-    console.log(error);    
-}
+
+
+
+
+
+
+let main = async () => {
+    try {
+        await connectDb(process.env.MONGO_URI);
+        app.listen(port, () => {
+            console.log('Server is running on http://localhost:' + port);
+        }
+        );
+    } catch (error) {
+        console.log(error);
+    }
 }
 
 main();
